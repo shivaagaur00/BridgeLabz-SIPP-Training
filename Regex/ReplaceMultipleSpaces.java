e69506170b9940a7fdf6b5c07ec234ecde76a891
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-08-19 00:37:50 +0530

    Completed Assignment

--- Regex/ReplaceMultipleSpaces.java
+++
@@ -1,7 +0,0 @@
-public class ReplaceMultipleSpaces {
-    public static void main(String[] args) {
-        String input = "This    is  an example     with multiple   spaces.";
-        String output = input.replaceAll("\s+", " ");
-        System.out.println(output);
-    }
-}
